24d864f6ab10b2dc723ce585c77e3ad51e9afdab
Peyman Mohtashami <p.mohtashami@gmail.com>
2025-12-06 18:12:16 +0100

    Add details base component

---
+++ src/app/admin/components/details/base-details.component.ts
@@ -0,0 +1,17 @@
+import {Component, input} from '@angular/core';
+import {DialogMode} from '../../enums/dialog';
+import {DetailType} from '../../enums/detail-type';
+
+@Component({
+  selector: 'app-details',
+  template: '',
+})
+export class BaseDetailsComponent<T> {
+  protected readonly DetailType = DetailType;
+
+  protected configService: any;
+
+  entity = input.required<T>();
+  dialogMode = input<DialogMode>();
+  detailType = input<DetailType>();
+}
