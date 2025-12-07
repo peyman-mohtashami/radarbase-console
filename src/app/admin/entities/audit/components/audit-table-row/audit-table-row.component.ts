e8d7d27d7beed21cc0856dbd28af1bfe7f2c82aa
Peyman Mohtashami <p.mohtashami@gmail.com>
2025-12-07 12:30:01 +0100

    Refactor audit and organization module to use the entity base component

--- src/app/admin/entities/audit/components/audit-table-row/audit-table-row.component.ts
+++ src/app/admin/entities/audit/components/audit-table-row/audit-table-row.component.ts
@@ -1,39 +1,22 @@
-import {Component, inject, input, signal} from "@angular/core";
-import {DetailType} from "../../../../enums/detail-type";
-import {DialogMode} from "../../../../enums/dialog";
+import {Component, inject} from "@angular/core";
 import {AppAudit} from "../../models/audit";
-import {MatCard} from "@angular/material/card";
 import {JsonPipe} from "@angular/common";
 import {LocalDateComponent} from "../../../../../core/locale/components/local-date/local-date.component";
-import {AuditDetailsComponent} from "../audit-details/audit-details.component";
-import {MatIconButton} from "@angular/material/button";
 import {AuditConfigService} from "../../services/audit-config.service";
+import {EntityComponent} from '../../../../components/entity/entity.component';
+import {BaseEntityComponent} from '../../../../components/entity/base-entity.component';
+import {AuditDetailsComponent} from '../audit-details/audit-details.component';
 
 @Component({
   selector: 'app-audit-table-row',
   templateUrl: './audit-table-row.component.html',
   imports: [
-    MatCard,
     LocalDateComponent,
     JsonPipe,
-    MatIconButton,
+    EntityComponent,
     AuditDetailsComponent,
   ]
 })
-export class AuditTableRowComponent {
-  protected readonly DialogMode = DialogMode;
-  protected readonly DetailType = DetailType;
-
-  configService = inject(AuditConfigService);
-
-  entity = input.required<AppAudit>();
-  extensionClass = input<string>();
-
-  expanded = signal(false);
-  updated = signal(false);
-
-  onExpansionClick(event: MouseEvent) {
-    event.stopPropagation();
-    this.expanded.update((currentValue) => !currentValue);
-  }
+export class AuditTableRowComponent extends BaseEntityComponent<AppAudit> {
+  override configService = inject(AuditConfigService);
 }
--- src/app/admin/entities/organization/components/organization-table-row/organization-table-row.component.ts
+++ src/app/admin/entities/organization/components/organization-table-row/organization-table-row.component.ts
@@ -1,18 +1,15 @@
-import {Component, effect, inject, input, signal} from "@angular/core";
+import {Component, inject} from "@angular/core";
 import {RouterLink} from "@angular/router";
 import {MatCard, MatCardContent} from "@angular/material/card";
-import {MatIconButton} from "@angular/material/button";
 
-import {DetailType} from "../../../../enums/detail-type";
 import {AppOrganization} from "../../models/organization";
-import {DialogMode} from "../../../../enums/dialog";
 import {OrganizationProjectsComponent} from "../organization-projects/organization-projects.component";
 import {OrganizationDetailsComponent} from "../organization-details/organization-details.component";
 import {ActionsComponent} from "../actions/actions.component";
-import {UpdateTrigger} from '../../services/organization-dialog.service';
 import {PermissionDirective} from "../../../../../core/auth/directives/show-if-has-role.directive";
-import {ROLES} from "../../../../../shared/enums/roles";
 import {OrganizationConfigService} from "../../services/organization-config.service";
+import {BaseEntityComponent} from '../../../../components/entity/base-entity.component';
+import {EntityComponent} from '../../../../components/entity/entity.component';
 
 @Component({
   selector: 'app-organization-table-row',
@@ -21,47 +18,13 @@ import {OrganizationConfigService} from "../../services/organization-config.serv
     MatCard,
     RouterLink,
     OrganizationProjectsComponent,
-    MatIconButton,
     OrganizationDetailsComponent,
     MatCardContent,
     ActionsComponent,
     PermissionDirective,
+    EntityComponent,
   ]
 })
-export class OrganizationTableRowComponent {
-  protected readonly ROLES = ROLES;
-  protected readonly DialogMode = DialogMode;
-  protected readonly DetailType = DetailType;
-
-  configService = inject(OrganizationConfigService);
-
-  entity = input.required<AppOrganization>();
-  entityUpdateTrigger= input<UpdateTrigger>();
-  extensionClass = input<string>();
-  gridView = input<boolean>(false);
-
-  expanded = signal(false);
-  updated = signal(false);
-
-  constructor() {
-    effect(() => {
-      const updateTrigger = this.entityUpdateTrigger();
-      if (!updateTrigger) return;
-
-      const {mode, entity} = updateTrigger;
-      if (entity.id !== this.entity().id) return;
-      if (mode === DialogMode.ADD || mode === DialogMode.EDIT) {
-        this.updated.set(true);
-        setTimeout(() => {
-          this.updated.set(false);
-        }, 1000);
-      }
-    });
-  }
-
-  onExpansionClick(event: MouseEvent) {
-    event.stopPropagation();
-    this.expanded.update((currentValue) => !currentValue);
-  }
-
+export class OrganizationTableRowComponent extends BaseEntityComponent<AppOrganization> {
+  override configService = inject(OrganizationConfigService);
 }
