4a6c3a3161dadab0bf90a6253226d4c4b415e0a5
Peyman Mohtashami <p.mohtashami@gmail.com>
2026-06-17 14:30:16 +0200

    Fix translations

--- src/app/admin/entities/configuration-scope/questionnaire/containers/questionnaire-dialog/containers/questionnaire-translation/questionnaire-translation.component.ts
+++ src/app/admin/entities/configuration-scope/questionnaire/containers/questionnaire-dialog/containers/questionnaire-translation/questionnaire-translation.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, input, OnInit, output, untracked} from '@angular/core';
+import {Component, input, OnInit, output} from '@angular/core';
 import {AppQuestionnaire, DEFAULT_LANGUAGE, ISO_LANGUAGES} from '../../../../models/questionnaire';
 import {FormArray, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {RadarOption} from '../../../../../../../../shared/components/mat-dynamic-input/mat-dynamic-input.component';
@@ -43,61 +43,6 @@ export class QuestionnaireTranslationComponent implements OnInit {
   });
 
   numberOfRows = 0;
-
-  // constructor() {
-  //   effect(() => {
-  //     const entity = untracked(() => this.entity());
-  //     if (entity) {
-  //       const t = entity.questions?.reduce((acc, curr) => {
-  //         return acc + 2 + (curr.field_note?.[entity.defaultLanguage.id] ? 1 : 0) + (curr.section_header?.[entity.defaultLanguage.id] ? 1 : 0) + (curr.select_choices_or_calculations?.length ?? 0);
-  //       }, 0);
-  //       this.numberOfRows = 6 + (this.entity()?.warningEnabled ? 1 : 0) + (t ?? 0);
-  //
-  //       const qs = entity.questions?.map(q => {
-  //         const choices = q.select_choices_or_calculations?.map(c => new FormGroup({
-  //           label: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //         })) ?? [];
-  //         const formGroup: any = {
-  //           field_label: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //           select_choices_or_calculations: new FormArray<FormGroup>(choices),
-  //         };
-  //         if (q.field_note?.[entity.defaultLanguage.id]) {
-  //           formGroup['field_note'] = new FormControl<Record<string, string>>({}, {nonNullable: true})
-  //         }
-  //         if (q.section_header?.[entity.defaultLanguage.id]) {
-  //           formGroup['section_header'] = new FormControl<Record<string, string>>({}, {nonNullable: true})
-  //         }
-  //         return new FormGroup(formGroup);
-  //       });
-  //
-  //       this.form = new FormGroup({
-  //         languages: new FormControl<RadarOption[]>([entity.defaultLanguage], {nonNullable: true}),
-  //         title: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //         description: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //         endText: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //         warn: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //         schedule: new FormGroup({
-  //           notification: new FormGroup({
-  //             title: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //             text: new FormControl<Record<string, string>>({}, {nonNullable: true}),
-  //           }),
-  //         }),
-  //         questions: new FormArray<FormGroup>(qs ?? []),
-  //
-  //       });
-  //
-  //       this.form.patchValue(entity);
-  //
-  //       this.form.valueChanges.pipe(
-  //         debounceTime(300)
-  //       ).subscribe(change => {
-  //         console.log('Class: QuestionnaireTranslationComponent, Function: , Line 98 change' , change);
-  //         this.changeEvent.emit(change);
-  //         this.valid.emit(this.form.valid);
-  //       });
-  //     }
-  //   });
-  // }
 
   ngOnInit() {
 
@@ -147,22 +92,8 @@ export class QuestionnaireTranslationComponent implements OnInit {
     this.form.valueChanges.pipe(
       debounceTime(300)
     ).subscribe(change => {
-      console.log('Class: QuestionnaireTranslationComponent, Function: , Line 98 change' , change);
       this.changeEvent.emit(change);
       this.valid.emit(this.form.valid);
     });
-
-    // this.form.valueChanges.pipe(
-    //   debounceTime(300)
-    // ).subscribe(change => {
-    //   console.log('Class: QuestionnaireTranslationComponent, Function: , Line 98 change' , change);
-    //   this.changeEvent.emit(change);
-    //   this.valid.emit(this.form.valid);
-    // });
-    //
-    // const entity = this.entity();
-    // if (entity) {
-    //   this.form.patchValue(entity);
-    // }
   }
 }
